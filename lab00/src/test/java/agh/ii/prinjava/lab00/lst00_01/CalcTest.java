0343bd4c3be01675d3a93aeb1c83221503ca027d
yhadded <haddedyassine78@gmail.com>
2025-10-28 17:42:37 +0100

    lab0 commit

---
+++ lab00/src/test/java/agh/ii/prinjava/lab00/lst00_01/CalcTest.java
@@ -0,0 +1,27 @@
+package agh.ii.prinjava.lab00.lst00_01;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("CalcTest.setUp...");
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("CalcTest.tearDown...");
+    }
+
+    @Test
+    void onePlusTwoIsThree() {
+        int a=1 ,b=2;
+        assertEquals(3,Calc.add(a,b));
+
+    }
+}
